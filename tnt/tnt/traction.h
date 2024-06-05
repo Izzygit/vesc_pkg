1bb2069e7d73668bf55c83269c1d3ecefd2c9717
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-06-05 16:11:00 -0700

    Update traction.h

--- tnt/tnt/traction.h
+++ tnt/tnt/traction.h
@@ -47,8 +47,7 @@ typedef struct {
 	float freq_factor;
 } TractionDebug;
 
-void check_traction(MotorData *m, TractionData *traction, State *state, RuntimeData *rt, tnt_config *config, TractionDebug *traction_dbg);
+void check_traction(MotorData *m, TractionData *traction, State *state, RuntimeData *rt, tnt_config *config, float inputtilt_interpolated, TractionDebug *traction_dbg);
 void reset_traction(TractionData *traction, State *state);
 void deactivate_traction(TractionData *traction, State *state, RuntimeData *rt, TractionDebug *traction_dbg);
 void configure_traction(TractionData *traction, tnt_config *config, TractionDebug *traction_dbg);
-void check_traction_braking(MotorData *m, TractionData *traction, State *state, RuntimeData *rt, tnt_config *config, float inputtilt_interpolated, TractionDebug *traction_dbg);
