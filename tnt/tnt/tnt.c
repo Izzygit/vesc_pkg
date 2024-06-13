1e908909602d57abd57292b5b90e2180d722e534
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-06-12 21:16:20 -0700

    Update tnt.c

--- tnt/tnt/tnt.c
+++ tnt/tnt/tnt.c
@@ -962,7 +962,7 @@ static void tnt_thd(void *arg) {
 			check_current(&d->motor, &d->surge, &d->state, &d->rt,  &d->tnt_conf); // Check for high current conditions
 			
 			// Modifiers to PID control
-			check_traction(&d->motor, &d->traction, &d->state, &d->rt, &d->tnt_conf, &d->traction_dbg);
+			check_traction(&d->motor, &d->traction, &d->state, &d->rt, &d->tnt_conf, d->remote.inputtilt_interpolated, &d->traction_dbg);
 			if (d->tnt_conf.is_surge_enabled)
 				check_surge(&d->motor, &d->surge, &d->state, &d->rt, &d->tnt_conf, &d->surge_dbg);
 
