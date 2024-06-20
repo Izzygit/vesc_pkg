7218caf57c3252280ae348244b0f0a3c07ce4ebc
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-06-19 18:40:27 -0700

    Update traction.h

--- tnt/tnt/traction.h
+++ tnt/tnt/traction.h
@@ -51,7 +51,6 @@ typedef struct {
 	float freq_factor;
 } TractionDebug;
 
-void update_erpm_sign(TractionData *traction, MotorData *m);
 void check_traction(MotorData *m, TractionData *traction, State *state, RuntimeData *rt, tnt_config *config, float inputtilt_interpolated, TractionDebug *traction_dbg);
 void reset_traction(TractionData *traction, State *state);
 void deactivate_traction(TractionData *traction, State *state, RuntimeData *rt, TractionDebug *traction_dbg, float exit);
