1bb5d1a093ce60b4eebe02adbbeb6f7ea1298cf2
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-07-08 17:08:56 -0700

    Update motor_data_tnt.h

--- tnt/tnt/motor_data_tnt.h
+++ tnt/tnt/motor_data_tnt.h
@@ -21,6 +21,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define ACCEL_ARRAY_SIZE 10 // For traction control erpm tracking
 #define ERPM_ARRAY_SIZE 10 // For traction control erpm tracking
 
 typedef struct {
