d7db79d1765cc6c037619dcdcc158447c2e6f117
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-07-15 18:30:31 -0700

    Update motor_data_tnt.c

--- tnt/tnt/motor_data_tnt.c
+++ tnt/tnt/motor_data_tnt.c
@@ -33,7 +33,8 @@ void motor_data_reset(MotorData *m) {
     m->last_accel = 0;
     m->last_erpm = 0;
     m->erpm_sign_soft = 0;
-	
+    m->duty_filtered = 0;
+	    
     m->erpm_idx = 0;
     for (int i = 0; i < ERPM_ARRAY_SIZE; i++) {
         m->erpm_history[i] = 0;
@@ -50,7 +51,7 @@ void motor_data_reset(MotorData *m) {
 
 void motor_data_configure(MotorData *m, tnt_config *config) {
     biquad_configure(&m->current_biquad, BQ_LOWPASS, 3.0 / config->hertz);
-    biquad_configure(&m->erpm_biquad, BQ_LOWPASS, 1.0 * config->wheelslip_filter_freq_fast / config->hertz);
+    biquad_configure(&m->erpm_biquad, BQ_LOWPASS, 1.0 * config->wheelslip_filter_freq / config->hertz);
     biquad_configure(&m->duty_biquad, BQ_LOWPASS, 3.0 / config->hertz);
    
     m->erpm_sign_factor = 0.9984 / config->hertz; //originally configured for 832 hz to delay an erpm sign change for 1 second (0.0012 factor)
