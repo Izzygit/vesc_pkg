b894ad8c8b8acaf3e358cdac7af2d370f23450b7
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-08-01 19:03:47 -0700

    Update runtime.c

--- tnt/tnt/runtime.c
+++ tnt/tnt/runtime.c
@@ -72,6 +72,8 @@ void yaw_reset(YawData *yaw, YawDebugData *yaw_dbg){
 }
 
 void reset_runtime(RuntimeData *rt) {
+	rt->setpoint = rt->pitch_angle;
+	
 	//Low pass pitch filter
 	rt->pitch_smooth = rt->pitch_angle;
 	biquad_reset(&rt->pitch_biquad);
