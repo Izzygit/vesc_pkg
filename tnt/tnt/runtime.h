d9aa208ec775a61a24271c77e7ca77aca8d1f52b
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-08-06 17:08:27 -0700

    Update runtime.h

--- tnt/tnt/runtime.h
+++ tnt/tnt/runtime.h
@@ -64,4 +64,4 @@ void runtime_data_update(RuntimeData *rt);
 void apply_pitch_filters(RuntimeData *rt, tnt_config *config);
 void calc_yaw_change(YawData *yaw, float yaw_angle, YawDebugData *yaw_dbg);
 void reset_runtime(RuntimeData *rt, YawData *yaw, YawDebugData *yaw_dbg);
-void configure_runtime(Runtime *rt, tnt_config *config);
+void configure_runtime(RuntimeData *rt, tnt_config *config);
