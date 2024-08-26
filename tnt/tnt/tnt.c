4e0b9609819f3c76aa4bb5a6667a17ae3457152d
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-08-25 17:17:01 -0700

    Update tnt.c

--- tnt/tnt/tnt.c
+++ tnt/tnt/tnt.c
@@ -691,8 +691,7 @@ static void brake(data *d) {
         d->brake_timeout = d->rt.current_time + brake_timeout_length;
     }
 
-    if (d->rt.current_time > d->brake_timeout ||
-      d->rt.current_time - d->rt.start_time < 5) {
+    if (d->rt.current_time > d->brake_timeout) {
         return;
     }
 
