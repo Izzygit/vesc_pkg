e2604cf05d882bd6fef7bde27d5f6db6affde1bd
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-08-29 17:14:06 -0700

    Update tnt.c

--- tnt/tnt/tnt.c
+++ tnt/tnt/tnt.c
@@ -273,7 +273,8 @@ static void reset_vars(data *d) {
 		
 		//Control variables
 		reset_pid(&d->pid);
-	
+		tone_reset(&d->tone);
+
 		//Remote
 		reset_remote(&d->remote, &d->st_tilt);
 	
@@ -533,8 +534,9 @@ static void calculate_setpoint_target(data *d) {
 	
 	//Duty FOC Tone
 	if (d->state.sat == SAT_PB_DUTY) {
-		if (d->tnt_conf.haptic_buzz_duty)
+		if (d->tnt_conf.haptic_buzz_duty) {
 			play_tone(&d->tone, d->tnt_conf.tone_freq_high_duty, d->tnt_conf.tone_volt_high_duty, 600);
+		}
 	} else if (d->tone.tone_in_progress && d->tone.duration == 600) {
 		end_tone(&d->tone);
 	}
