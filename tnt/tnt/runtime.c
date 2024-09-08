e5a9d8c5aa9171823fae2ff2e442c17883a32e86
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-09-07 20:07:04 -0700

    Update runtime.c

--- tnt/tnt/runtime.c
+++ tnt/tnt/runtime.c
@@ -93,10 +93,10 @@ void configure_runtime(RuntimeData *rt, tnt_config *config) {
 	rt->motor_timeout_s = 20.0f / config->hertz;
 	
 	//Pitch Biquad Configure
-	biquad_configure(&d->rt.pitch_biquad, BQ_LOWPASS, 1.0 * d->tnt_conf.pitch_filter / d->tnt_conf.hertz);
+	biquad_configure(rt->pitch_biquad, BQ_LOWPASS, 1.0 * config->pitch_filter / d->tnt_conf.hertz);
 
 	//Pitch Kalman Configure
-	configure_kalman(&d->tnt_conf, &d->rt.pitch_kalman);
+	configure_kalman(config, rt->pitch_kalman);
 }
 
 void ride_timer(RideTimeData *ridetimer, RuntimeData *rt){
