1c344ae9c9ecb63df5c03582abc92373a11f6164
Izzygit <126716104+Izzygit@users.noreply.github.com>
2024-09-13 21:20:31 -0700

    Update surge.c

--- tnt/tnt/surge.c
+++ tnt/tnt/surge.c
@@ -70,7 +70,7 @@ void check_surge(MotorData *m, SurgeData *surge, State *state, RuntimeData *rt, 
 	}
 }
 
-void check_current(MotorData *m, SurgeData *surge, State *state, tnt_config *config, ToneData *tone, ToneConfig *toneconfig, RuntimeData *rt) {
+void check_current(MotorData *m, SurgeData *surge, State *state, tnt_config *config, ToneData *tone, ToneConfig *toneconfig) {
 	float scale_start_current = lerp(1.0 * config->surge_scaleduty / 100.0, .95, config->surge_startcurrent, config->surge_start_hd_current, m->duty_cycle);
 	surge->start_current = fminf(config->surge_startcurrent, scale_start_current); 
 	if ((m->current_filtered * m->erpm_sign > surge->start_current - config->overcurrent_margin) && 	//High current condition 
