dde2ef50a50bca2bc50e957fa7044b598752025b
Izzygit <126716104+Izzygit@users.noreply.github.com>
2026-06-09 19:26:07 -0700

    Update runtime.c

--- tnt/tnt/runtime.c
+++ tnt/tnt/runtime.c
@@ -117,7 +117,7 @@ void configure_runtime(RuntimeData *rt, tnt_config *config) {
 	configure_kalman(config, &rt->pitch_kalman);
 
 	//Yaw change correction factor
-	rt->imu_rate_factor = 1 //lerp(832, 10000, 1, 2, config->hertz);
+	rt->imu_rate_factor = 1; //lerp(832, 10000, 1, 2, config->hertz);
 	
 	// EMA Filter Factor
 	//float imu_sample_rate = VESC_IF->get_cfg_int(CFG_PARAM_IMU_sample_rate);
