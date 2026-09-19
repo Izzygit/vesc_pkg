9a97d83231e850fb42b96da9c7723a2f0c216818
Izzygit <126716104+Izzygit@users.noreply.github.com>
2026-09-18 18:49:06 -0700

    Refactor ride tracking configuration call
    Removed redundant configuration call for ride tracking.

--- tnt/tnt/tnt.c
+++ tnt/tnt/tnt.c
@@ -103,7 +103,7 @@ static void configure(data *d) {
 	tone_configure_all(&d->tone_config, &d->tnt_conf, &d->tone);	//FOC play tones
 	configure_ride_tracking(&d->ridetrack, &d->tnt_conf);			//Ride tracking
 	reset_ride_tracking_on_configure(&d->ridetrack, &d->tnt_conf, &d->traction_dbg);	//Reset current trip information
-	configure_ride_tracking(&d->ridetrack, &d->tnt_conf);			//Ride tracking
+	configure_ride_tracking(&d->ridetrack);			//Ride tracking
 	
 	//initialize pitch arrays for acceleration
 	angle_kp_reset(&d->accel_kp);
